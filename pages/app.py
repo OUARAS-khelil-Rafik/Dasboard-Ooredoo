7b8e10fc282d8dadcd15cf637918868cd6dc53c6
OUARAS-khelil-Rafik <k.ouaras@univ-boumerdes.dz>
2025-05-01 22:31:11 +0100

    Update

--- pages/app.py
+++ pages/app.py
@@ -5,8 +5,6 @@ st.set_page_config(
     page_title="Dashboard Ooredoo",
     page_icon=":red_circle:",
     layout="centered",
-    initial_sidebar_state="collapsed",
 )
 
 st.title("Bienvenue dans le Dashboard Ooredoo")
-st.write("Contenu principal ici...")
